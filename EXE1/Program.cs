8987ce12c72e9cc975950b0e46e1d743cae837dd
Mei Tri <mei.tri.ft21@mail.umy.ac.id>
2022-10-13 17:28:39 +0700

    Add project files.

---
+++ EXE1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE1
+{
+    class Program
+    {
+        //deklrasi array int dengan ukuran 32
+        private int[] MEI = new int[32];
+
+        //deklarasi variabel int untuk menyimpan banyaknya data yang disimpan pada array
+        private int n;
+
+        //fungsi / method untuk menerima masukan
+        public void read()
+        {
+            //menerima angka untuk menentukan banyaknya data yang disimpan pada array 
+            while (true)
+            {
+                Console.Write("masukkan banyaknya elemen pada array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 32)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 32 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Masukkan elemen array ");
+            Console.WriteLine("-----------------------");
+
+            // Pengguna memasukkan elemen pada array 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                MEI[i] = Int32.Parse(s1);
+            }
+
+        }
+        public void display()
+        {
+            // Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Menampilkan array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int MT = 0; MT < n; MT++)
+            {
+                Console.WriteLine(this.MEI[MT]);
+            }
+            Console.WriteLine("");
+        }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // for n - 1 passes
+            {
+                //pada pass i, bandingkan n - 1 elemen pertama dengan elemen selanjutnya
+                for (int MC = 0; MC < n; MC++)
+                {
+                    if (this.MEI[MC] > this.MEI[MC + 1]) // jika elemen tidak dalam urutan yang benar
+                    {
+                        // tukar elemen
+                        int temp;
+                        temp = this.MEI[MC];
+                        this.MEI[MC] = this.MEI[MC + 1];
+                        this.MEI[MC + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // creating the object of the BubbleSort class
+            Program mylist = new Program();
+
+            //pemanggilan fungsi untuk menerima elemen array
+            mylist.read();
+
+            //pemanggilan fungsi untuk mengurutkan array 
+            mylist.BubbleSortArray();
+
+            //pemanggilan fungsi untuk menampilkan array yang tersusun
+            mylist.display();
+
+            //exit
+            Console.WriteLine("\n\n Tekan enter untuk keluar.");
+            Console.Read();
+        }
+    }
+}
+
